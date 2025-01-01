9a753eeae1d6c171644ac6bf4efb38364096e6d2
SpencerRaw <spencerxu79@gmail.com>
2025-01-01 21:52:06 +0800

    sample tickets system

---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,20 @@
+import { initialTickets } from "@/data";
+import Link from "next/link";
+import { ticketPath } from "@/path";
+
+const TicketsPage = () => {
+  return (
+    <div>
+      {initialTickets.map((ticket) => (
+        <div key={ticket.id}>
+          <h2 className="text-lg">{ticket.title}</h2>
+          <Link href={ticketPath(ticket.id)} className="text-sm underline">
+            View
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TicketsPage;
---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const initialTickets = [
+    {
+        id:"1",
+        title:"Ticket 1",
+        content:"This is first ticket.",
+        status:"DONE",
+    },
+    {
+        id:"2",
+        title:"Ticket 2",
+        content:"This is second ticket.",
+        status:"OPEN",
+    }
+]
